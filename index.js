22844ee0e322b9b00f35dc0862df3ab1136458a4
DanielKanyo <danielkanyo992@gmail.com>
2017-08-16 19:22:22 +0200

    a get request

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 //set up express app
 const app = express();
 
+//get request
+app.get('/api', function(req, res) {
+  console.log('GET request');
+  res.send({name: 'Beatrix'});
+});
+
 //listen for requests
 app.listen(process.env.port || 6060, function() {
   console.log('Now listening for requests...');
