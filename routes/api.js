023416d540b07569590e3877107a2e542b390482
DanielKanyo <danielkanyo992@gmail.com>
2017-08-21 22:21:30 +0200

    body-parser

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,12 @@ router.get('/ninjas', function(req, res) {
 
 //add a new ninja to the database
 router.post('/ninjas', function(req, res) {
-  res.send({type: 'POST'});
+  console.log(req.body);
+  res.send({
+    type: 'POST',
+    name: req.body.name,
+    job: req.body.job
+  });
 });
 
 //update a ninja in the database
